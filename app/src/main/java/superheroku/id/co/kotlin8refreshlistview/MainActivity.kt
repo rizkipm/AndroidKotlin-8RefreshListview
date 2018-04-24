cb3ed29e14bca12e89f3767829238458c40fb33f
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 21:40:22 +0700

    add

---
+++ app/src/main/java/superheroku/id/co/kotlin8refreshlistview/MainActivity.kt
@@ -0,0 +1,12 @@
+package superheroku.id.co.kotlin8refreshlistview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

f5dacbfde3e517caa7b322dd0ed5e65086158cf5
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 21:40:39 +0700

    add

--- app/src/main/java/superheroku/id/co/kotlin8refreshlistview/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/kotlin8refreshlistview/MainActivity.kt
@@ -1,9 +1,14 @@
 package superheroku.id.co.kotlin8refreshlistview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary",
+            "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich",
+            "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
